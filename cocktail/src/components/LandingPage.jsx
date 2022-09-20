b61fbe04021dad797dd56557ce393e9117cae4f3
Nathanael Kim <nateykim@gmail.com>
2022-09-20 00:49:44 -0700

    Added some mobile responsiveness

--- cocktail/src/components/LandingPage.jsx
+++ cocktail/src/components/LandingPage.jsx
@@ -11,11 +11,13 @@ function LandingPage() {
     >
         <div class="hero"> 
             <div class="container spacing">
-                <h1 class="main=text">Your New</h1>
-                <h1 class="main=text">FAVORITE Drink</h1>
-                <p>Whether you are experienced or are just starting out,</p>
-                <p>have confidence making the latest and tastiest drinks!</p>
-                <p>You might even find your new favorite cocktail</p>
+                <h1 class="main=text">Your New <br/> FAVORITE Drink</h1>
+                {/* <h1 class="main=text">FAVORITE Drink</h1> */}
+                <p>Whether you are experienced or are just starting out, 
+                <br/>have confidence making the latest and tastiest drinks! 
+                <br/>You might even find your new favorite cocktail</p>
+                {/* <p>have confidence making the latest and tastiest drinks!</p>
+                <p>You might even find your new favorite cocktail</p> */}
             </div>
         </div>
     </motion.div>
