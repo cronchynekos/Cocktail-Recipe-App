42eb157ffb4537db5bc8af7c9721c2b963f6f69c
Nathanael Kim <nateykim@gmail.com>
2022-09-20 01:27:08 -0700

    Styling

--- cocktail/src/components/LandingPage.jsx
+++ cocktail/src/components/LandingPage.jsx
@@ -12,12 +12,9 @@ function LandingPage() {
         <div class="hero"> 
             <div class="container spacing">
                 <h1 class="main=text">Your New <br/> FAVORITE Drink</h1>
-                {/* <h1 class="main=text">FAVORITE Drink</h1> */}
                 <p>Whether you are experienced or are just starting out, 
                 <br/>have confidence making the latest and tastiest drinks! 
                 <br/>You might even find your new favorite cocktail</p>
-                {/* <p>have confidence making the latest and tastiest drinks!</p>
-                <p>You might even find your new favorite cocktail</p> */}
             </div>
         </div>
     </motion.div>
